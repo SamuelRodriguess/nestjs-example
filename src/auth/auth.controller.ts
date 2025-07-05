a8c8b2ff4ef0d972ed7bc04d4489e70fb5bec95e
samuelrodrigues <samuelrodriguessouzaiphone@gmail.com>
2025-07-05 00:37:20 -0300

    feat: resource to post service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('login')
-  login() {}
+  login() {
+    return this.authService.islogin;
+  }
 }
