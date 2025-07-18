d636bb463b21b592c8203d9b81fd4d8138c72e2b
samuelrodrigues <samuelrodriguessouzaiphone@gmail.com>
2025-07-18 11:41:20 -0300

    feat: user service with create method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from 'src/common/hashing/ hashing.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await this.hashingService.hash(
+      createUserDto.password,
+    );
+
+    const newUser = {
+      name: createUserDto.name,
+      email: createUserDto.email,
+      password: hashedPassword,
+    };
+
+    console.log('newUser:', newUser);
+
+    const created = await this.userRepository.save(newUser);
+    return created;
   }
 
   findAll() {
