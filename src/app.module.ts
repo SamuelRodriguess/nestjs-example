97ae7b081587a13753a76063fcb05e957f73d059
samuelrodrigues <samuelrodriguessouzaiphone@gmail.com>
2025-07-18 09:54:52 -0300

    feat: created user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, PostModule, UserModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    AuthModule,
+    PostModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'sqlite',
+      database: process.env.DB_DATABASE,
+      synchronize: process.env.DB_SYNCHRONIZE === '1',
+      autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+    }),
+  ],
 })
 export class AppModule {}
