a4af66ee4c06eb6213113cb2eae266f08667892c
samuelrodrigues <samuelrodriguessouzaiphone@gmail.com>
2025-07-24 10:19:41 -0300

    feat: function to  handle error

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class LoginDTO {
-  username: string;
+  @IsEmail({}, { message: 'E-mail inválido' })
+  email: string;
+
+  @IsString({ message: 'Senha deve ser uma string' })
+  @IsNotEmpty({ message: 'Senha não pode estar vazia' })
   password: string;
 }
